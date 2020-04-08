8b9be5d625cac3cab695e380e04cba0cbaff9175
Thomas <lythomm@gmail.com>
2020-04-08 11:17:08 +0200

    tableau pays / capitales

--- Quizz/quasar.conf.js
+++ Quizz/quasar.conf.js
@@ -42,7 +42,7 @@ module.exports = function (ctx) {
       //            (not treeshaking Quasar; biggest bundle size; convenient)
       all: 'auto',
 
-      components: ["QBanner","QChip",'QBtn'],
+      components: ["QBanner","QBtn"],
       directives: [],
 
       // Quasar plugins
--- Quizz/src/pages/quizz.vue
+++ Quizz/src/pages/quizz.vue
@@ -2,28 +2,41 @@
     <q-page padding>
 
         <div class="col-sm-9">
+            <!--
             <h3>Quelle est la capitale du pays : France ?</h3>
-
+            <ul>
+              <li v-for="pays in paysCapitales" :key="pays.paysCapitales">{{ pays }}</li>
+            </ul>
+            -->
             <div class="row q-gutter-lg">
                 <div class="col-8 col-sm-6">
-                    <q-btn id="Paris" color="white" text-color="black" label="Paris" v-on:click="clickReponse('paris')"/>
+                    <q-btn color="white" text-color="black" v-bind:label=paysCapitales[1][0] v-on:click="clickReponse('paris')"/>
                 </div>
                 <div class="col-4 col-sm-6">
-                    <q-btn id="Monaco" color="white" text-color="black" label="Monaco" v-on:click="clickReponse('monaco')"/>
+                    <q-btn color="white" text-color="black" v-bind:label=paysCapitales[2][0] v-on:click="clickReponse('monaco')"/>
                 </div>
                 <div class="col-8 col-sm-6">
-                    <q-btn id="Madrid" color="white" text-color="black" label="Madrid" v-on:click="clickReponse('madrid')"/>
+                    <q-btn color="white" text-color="black" v-bind:label=paysCapitales[3][0] v-on:click="clickReponse('madrid')"/>
                 </div>
                 <div class="col-4 col-sm-6">
-                    <q-btn id="Barcelone" color="white" text-color="black" label="Barcelone" v-on:click="clickReponse('barcelone')"/>
+                    <q-btn color="white" text-color="black" v-bind:label=paysCapitales[4][0] v-on:click="clickReponse('barcelone')"/>
                 </div>
             </div>
         </div>
+
     </q-page>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      paysCapitales: [
+        ['France', 'Espagne', 'Italie', 'Angleterre', 'Autriche'],
+        ['Paris', 'Madrid', 'Rome', 'Londres', 'Viennes']
+      ]
+    }
+  },
   methods: {
     clickReponse: function (reponse) {
       if (reponse === 'paris') {
