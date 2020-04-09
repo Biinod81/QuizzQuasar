b5ec02a4c7bae1462d582d6d0a7b597454d93c70
Thomas <lythomm@gmail.com>
2020-04-09 15:58:37 +0200

    bouton ajouter + ajustement du nb de boutons

--- Quizz/src/pages/quizzV2.vue
+++ Quizz/src/pages/quizzV2.vue
@@ -3,8 +3,14 @@
 
     <div class="col-sm-9" @keyup.enter=verificationReponse(reponseUtilisateur)>
         <q-btn style="background: goldenrod; color: white" label="START / SUIVANT" @click="choixPaysAleatoire(); viderChamp()"/>
-        <q-btn style="background: goldenrod; color: white" label="RESET" @click="reset(); viderChamp()"/>
+        <q-btn style="background: #FF0080; color: white" label="RESET" @click="reset(); viderChamp()"/>
         <h3>Quelle est la capitale du pays : {{ pays }}</h3>
+
+        <div class="q-gutter-y-md" style="max-width: 600px">
+            <q-tabs v-for="indexBtn in NB_BTN" :key="indexBtn">
+              <q-btn v-model="reponseUtilisateur" color="white" text-color="black" :label=paysCapitales[indexBtn-1].capitale @click="verificationReponse(paysCapitales[indexBtn-1].capitale)" />
+            </q-tabs>
+        </div>
 
         <div class="q-gutter-md" style="max-width: 300px">
             <q-input outlined label="Entrez votre réponse ici" v-model="reponseUtilisateur"/>
@@ -21,13 +27,6 @@
             </q-field>
         </div>
 
-        <div class="q-gutter-y-md" style="max-width: 600px">
-            <q-tabs
-
-            >
-                <q-btn color="white" text-color="black" label="test" />
-            </q-tabs>
-        </div>
     </div>
 
   </q-page>
@@ -37,7 +36,7 @@
 export default {
   data () {
     return {
-      TabPaysCapitales: [
+      paysCapitales: [
         { pays: 'France', capitale: 'Paris' },
         { pays: 'Espagne', capitale: 'Madrid' },
         { pays: 'Angleterre', capitale: 'Londres' },
@@ -53,49 +52,52 @@ export default {
         { pays: 'Corée du Sud', capitale: 'Séoul' }
       ],
       paysDejaSelectionne: [], // stock l'index des pays déjà sélectionnés
-      NB_QUESTION: 10,
+      NB_QUESTION: 10, // Nombre total de questions posées
+      NB_BTN: 4, // Nombre de boutons dispo pour l'utilisateur
       reponseUtilisateur: '', // réponse de l'utilisateur
       reponseQuestion: '', // réponse de la réponse
-      index: 0,
+      index: 0, // index permettant de choisir les capitales / pays
       pays: '',
       capitale: ''
     }
   },
   methods: {
+
     // vérifie la réponse de l'utilisateur et show un text en fonction de la réponse
     verificationReponse: function (reponse) {
-      if (this.reponseUtilisateur.toUpperCase() === this.capitale.toUpperCase()) {
+      if (this.reponse.toUpperCase() === this.capitale.toUpperCase()) {
         this.reponseQuestion = 'Bien joué !'
       } else {
         this.reponseQuestion = 'Dommage, il fallait répondre : ' + this.capitale
       }
     },
 
-    // Choisis aléatoirement un pays parmis le tableau
+    // Choisis aléatoirement un pays parmis le tableau d'objet "paysCapitales"
     choixPaysAleatoire () {
-      // Permet de ne pas choisir 2 fois le même pays
-      do {
-        this.index = Math.floor(Math.random() * Math.floor(this.TabPaysCapitales.length)) // Choisis un index aléatoirement
+      do { // Permet de ne pas choisir 2 fois le même pays
+        this.index = Math.floor(Math.random() * Math.floor(this.paysCapitales.length)) // Choisis un index aléatoirement
+        console.log('Index sélectionné : ' + this.index) // renvoie l'index qui a été choisis
       } while (this.paysDejaSelectionne.includes(this.index))
 
       // ajoute l'index choisis aléatoirement dans le tableau paysDejaSelectionné
       this.paysDejaSelectionne.push(this.index)
-      console.log('Index des Pays déjà sélectionné ' + this.paysDejaSelectionne)
+      console.log('Index des Pays déjà sélectionné : ' + this.paysDejaSelectionne)
 
-      this.capitale = this.TabPaysCapitales[this.index].capitale // renvoie les capitales
-      this.pays = this.TabPaysCapitales[this.index].pays // renvoie les pays
-      // renvoie l'index / le pays / la capitale dans la console (pour les tests)
-      console.log('Index : ' + this.index) // renvoie l'index actuel
-      console.log('capitales : ' + this.capitale)
-      console.log('pays : ' + this.pays)
+      this.capitale = this.paysCapitales[this.index].capitale // attribut la capitale choisie aléatoirement à la variable "capitale"
+      this.pays = this.paysCapitales[this.index].pays // attribut le pays choisi aléatoirement à la variable "pays"
+      console.log('capitales : ' + this.capitale) // renvoie la capitale qui a été choisie
+      console.log('pays : ' + this.pays) // renvoie la pays qui a été choisi
     },
+
     // Vide les champs explication et reponse pour que ce soit moins redondant pour l'utilisateur
     viderChamp () {
       this.reponseUtilisateur = ''
       this.reponseQuestion = ''
     },
-    // vide le tableau des pays déjç choisis
+
+    // vide le tableau des pays déjà choisis, remet des valeurs null pour "capitale" et "pays" et clear la console
     reset () {
+      console.clear()
       this.paysDejaSelectionne.length = 0
       this.capitale = ''
       this.pays = ''
